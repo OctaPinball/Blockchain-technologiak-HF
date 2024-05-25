db30c570616d60ca05ad3acc8d9468da4b9529db
Scholtz Bálint András <scholtz.balint@gmail.com>
2024-05-25 16:20:14 +0200

    Update Crossing.js

--- test/Crossing.js
+++ test/Crossing.js
@@ -1,126 +1,81 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("TrainCrossing", function () {
+    let TrainCrossing, trainCrossing, owner, addr1, addr2, addr3;
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
-
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+    beforeEach(async function () {
+        TrainCrossing = await ethers.getContractFactory("TrainCrossing");
+        [owner, addr1, addr2, addr3] = await ethers.getSigners();
+        trainCrossing = await TrainCrossing.deploy(600, 5, 300);
+        await trainCrossing.deployed();
     });
 
     it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
+        expect(await trainCrossing.infrastructure()).to.equal(owner.address);
     });
 
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
+    it("Should authorize a train and allow it to request crossing", async function () {
+        await trainCrossing.authorizeTrain(addr1.address);
+        expect(await trainCrossing.authorizedTrains(addr1.address)).to.equal(true);
 
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
+        await expect(trainCrossing.connect(addr1).requestTrainCrossing())
+            .to.emit(trainCrossing, "TrainCrossingRequest")
+            .withArgs(addr1.address, true);
     });
 
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
-  });
-
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
+    it("Should not allow unauthorized train to request crossing", async function () {
+        await expect(trainCrossing.connect(addr1).requestTrainCrossing())
+            .to.be.revertedWith("Not authorized train.");
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    it("Should allow a car to request and release crossing permission", async function () {
+        await trainCrossing.updateFreeToCrossState();
+        await expect(trainCrossing.connect(addr1).requestCarPermission())
+            .to.emit(trainCrossing, "CarCrossingPermissionGranted")
+            .withArgs(addr1.address);
 
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
+        await expect(trainCrossing.connect(addr1).releaseCarPermission())
+            .to.emit(trainCrossing, "CarCrossingPermissionReleased")
+            .withArgs(addr1.address);
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
+    it("Should not allow a car to request crossing when it's full", async function () {
+        await trainCrossing.updateFreeToCrossState();
+        for (let i = 0; i < 5; i++) {
+            await trainCrossing.connect(addr1).requestCarPermission();
+        }
+        await expect(trainCrossing.connect(addr2).requestCarPermission())
+            .to.be.revertedWith("Crossing is full.");
     });
-  });
+
+    it("Should allow authorized train to release crossing", async function () {
+        await trainCrossing.authorizeTrain(addr1.address);
+        await trainCrossing.connect(addr1).requestTrainCrossing();
+        await expect(trainCrossing.connect(addr1).releaseTrainCrossing())
+            .to.emit(trainCrossing, "TrainCrossingPermissionReleased")
+            .withArgs(addr1.address);
+    });
+
+    it("Should update crossing state to free to cross", async function () {
+        await trainCrossing.updateFreeToCrossState();
+        expect(await trainCrossing.crossingState()).to.equal(0); // FREE_TO_CROSS
+    });
+
+    it("Should lock crossing after validity period", async function () {
+        await trainCrossing.updateFreeToCrossState();
+        await ethers.provider.send("evm_increaseTime", [601]); // Increase time by 601 seconds
+        await trainCrossing.connect(addr1).requestCarPermission();
+        expect(await trainCrossing.crossingState()).to.equal(2); // PRE_LOCKED
+    });
+
+    it("Should stop train if pre-locked time has passed", async function () {
+        await trainCrossing.authorizeTrain(addr1.address);
+        await trainCrossing.updateFreeToCrossState();
+        await trainCrossing.connect(addr1).requestTrainCrossing();
+        await ethers.provider.send("evm_increaseTime", [301]); // Increase time by 301 seconds
+        await expect(trainCrossing.connect(addr1).requestTrainCrossing())
+            .to.emit(trainCrossing, "StopTrain")
+            .withArgs(addr1.address);
+    });
 });
